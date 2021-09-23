1d0b4c8c9110d72708802656859d095d91444d10
Matheus Santos <matheussm301@gmail.com>
2021-09-23 19:18:20 -0300

    add mongo db

--- src/players/players.service.ts
+++ src/players/players.service.ts
@@ -1,7 +1,6 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { v4 as uuid } from 'uuid';
 
 import { CreatePlayerDTO } from './dtos/createPlayer.dto';
 import { Player } from './interfaces/player.interface';
@@ -41,11 +40,11 @@ export class PlayersService {
   }
 
   async get(): Promise<Player[]> {
-    return this.players;
+    return this.playerModel.find();
   }
 
   async findByEmail(email: string): Promise<Player> {
-    const player = this.players.find((player) => player.email === email);
+    const player = await this.playerModel.findOne({ email });
 
     if (!player) {
       throw new NotFoundException(`Player not found`);
@@ -57,7 +56,7 @@ export class PlayersService {
   async deletePlayer(email: string): Promise<void> {
     const playerExists = await this.findByEmail(email);
 
-    this.players = this.players.filter((player) => player !== playerExists);
+    await playerExists.delete();
   }
 
   private async create(data: CreatePlayerDTO): Promise<Player> {
@@ -71,9 +70,13 @@ export class PlayersService {
     name,
     phone,
   }: IRequestUpdatePlayer): Promise<Player> {
-    player.name = name;
-    player.phone = phone;
-
-    return player;
+    const result = await this.playerModel.findOneAndUpdate(
+      { email: player.email },
+      {
+        name,
+        phone,
+      },
+    );
+    return result;
   }
 }
