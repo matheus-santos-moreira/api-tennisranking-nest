770d064579f52cd91e22749d0ee7172babb1f88b
Matheus Santos <matheussm301@gmail.com>
2021-09-23 19:36:26 -0300

    add class validaor and use nest pipes

--- src/players/dtos/createPlayer.dto.ts
+++ src/players/dtos/createPlayer.dto.ts
@@ -1,5 +1,12 @@
+import { IsEmail, IsNotEmpty, IsPhoneNumber } from 'class-validator';
+
 export class CreatePlayerDTO {
+  @IsPhoneNumber()
   readonly phone: string;
+
+  @IsEmail()
   readonly email: string;
+
+  @IsNotEmpty()
   readonly name: string;
 }
