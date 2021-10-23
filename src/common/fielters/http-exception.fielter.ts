95f6e48d4118b66f14b734b945910ddce61e8640
Matheus Santos <matheussm301@gmail.com>
2021-10-23 13:08:14 -0300

    chore: update exception

--- src/common/fielters/http-exception.fielter.ts
+++ src/common/fielters/http-exception.fielter.ts
@@ -19,16 +19,13 @@ export class AllExceptionsFilters implements ExceptionFilter {
       exception instanceof HttpException ? exception.getStatus() : 500;
 
     const message =
-      exception instanceof HttpException
-        ? exception.getResponse()
-        : 'Internal Server Error';
+      exception instanceof HttpException ? exception.getResponse() : exception;
 
     this.logger.error(
       `Http Status: ${status} Error Message: ${JSON.stringify(message)}`,
     );
 
     response.status(status).json({
-      statusCode: status,
       timestamp: new Date().toISOString(),
       path: request.url,
       error: message,
